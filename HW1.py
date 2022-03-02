5d4e8615db39bfd5f9da5a39608c9466ed67732d
RonyHacham <100578779+RonyHacham@users.noreply.github.com>
2022-03-02 14:02:16 +0200

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,18 @@
+def my_func(x1,x2,x3):
+    if ((x1+x2+x3)==0):
+        return ("Not a number – denominator equals zero")
+    if((type(x1)!=float) or (type(x2)!=float) or (type(x2)!=float)):
+        return ("Error: parameters should be float")
+    func=((x1+x2+x3)*(x2+x3)*x3)/(x1+x2+x3)
+    return (float(func))
+
+
+def convert(hours, minutes):
+    if ((type(hours)==str) or(type(minutes)==str)):
+        return ("Input error!")
+    if ((hours<0) or (minutes<0)):
+        return ("Input error!")
+    solu= (hours*60*60)+(minutes*60)
+    return (solu)
+
+
