d0dc9112aad7a77e7505dd27bebdbc2ea3e2a423
RonyHacham <100578779+RonyHacham@users.noreply.github.com>
2022-03-22 15:33:24 +0200

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -1,8 +1,8 @@
 def my_func(x1,x2,x3):
+    if((type(x1)!=float) or (type(x2)!=float) or (type(x3)!=float)):
+        return ("Error: parameters should be float")
     if ((x1+x2+x3)==0):
         return ("Not a number – denominator equals zero")
-    if((type(x1)!=float) or (type(x2)!=float) or (type(x2)!=float)):
-        return ("Error: parameters should be float")
     func=((x1+x2+x3)*(x2+x3)*x3)/(x1+x2+x3)
     return (float(func))
 
